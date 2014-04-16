9159455eb4d025bdead36204216547269de0cf06
phpusr <phpusr@gmail.com>
2014-04-16 20:36:51 +0600

    add: IdTableAbstract

---
+++ src/main/scala/mpt/lab/one/IdTableAbstract.scala
@@ -0,0 +1,14 @@
+package mpt.lab.one
+
+/**
+ * @author phpusr
+ *         Date: 16.04.14
+ *         Time: 20:27
+ */
+
+/**
+ * Абстрактная таблица идентификаторов
+ */
+abstract class IdTableAbstract {
+
+}
