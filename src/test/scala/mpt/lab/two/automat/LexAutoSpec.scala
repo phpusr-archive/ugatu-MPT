4e6806f9a356425e53f76f4371ff4728fd4b60f2
phpusr <phpusr@gmail.com>
2014-05-01 10:58:34 +0600

    add: Проверка обработки числовых констант

--- src/test/scala/mpt/lab/two/automat/LexAutoSpec.scala
+++ src/test/scala/mpt/lab/two/automat/LexAutoSpec.scala
@@ -151,7 +151,7 @@ class LexAutoSpec extends FlatSpec {
   //------------------ State V ------------------//
 
   /** Проверка обработки идентификаторов */
-  it should "" in {
+  it should "processing id" in {
     println("\n>> State V")
 
     val out = ListBuffer[LexElem]()
@@ -206,6 +206,67 @@ class LexAutoSpec extends FlatSpec {
     }
 
     assert(out.size == outSize)
-
   }
+
+
+  //------------------ State D ------------------//
+
+  /** Проверка обработки числовых констант */
+  it should "processing digit" in {
+    println("\n>> State D")
+
+    val out = ListBuffer[LexElem]()
+    val auto = new LexAuto
+
+    // Добавление цифр к числу
+    auto.makeLexList(Array("0123456789"), out)
+    assert(auto.currentState == AutoPos.D)
+
+    // Начало комментария
+    auto.makeLexList(Array("1{"), out)
+    assert(auto.currentState == AutoPos.C)
+    assert(out(0).lexInfo.name == LexType.Const)
+    assert(out(0).lexInfo.info == "1")
+
+    // Начало присваивания
+    auto.makeLexList(Array("2:"), out)
+    assert(auto.currentState == AutoPos.G)
+    assert(out(1).lexInfo.name == LexType.Const)
+    assert(out(1).lexInfo.info == "2")
+
+    assert(out.size == 2)
+
+    // Незначащие символы
+    var eIndex = 2
+    "(,).;".foreach { e =>
+      auto.makeLexList(Array("3" + e.toString), out)
+      assert(auto.currentState == AutoPos.F)
+      assert(out(eIndex).lexInfo.name == LexType.Const)
+      assert(out(eIndex).lexInfo.info == "3")
+      assert(out(eIndex+1).lexInfo.name == LexType.MeaninglessSymbol)
+      assert(out(eIndex+1).lexInfo.info == e.toString)
+
+      eIndex += 2
+    }
+
+    // Символы-разделители
+    " \t".foreach { e =>
+      auto.makeLexList(Array("4" + e.toString), out)
+      assert(out(eIndex).lexInfo.name == LexType.Const)
+      assert(out(eIndex).lexInfo.info == "4")
+
+      eIndex += 1
+    }
+
+    // Не поддерживаемые символы
+    val outSize = out.size
+    "!№%?*".foreach { e =>
+      intercept[MatchError] {
+        auto.makeLexList(Array("5" + e.toString), out)
+      }
+    }
+
+    assert(out.size == outSize)
+  }
+
 }
