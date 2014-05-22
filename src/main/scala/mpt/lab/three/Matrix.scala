6508c948de0404bb61085b472999c7183061f3e4
phpusr <phpusr@gmail.com>
2014-05-22 16:42:12 +0600

    add: Matrix, SyntSymb

---
+++ src/main/scala/mpt/lab/three/Matrix.scala
@@ -0,0 +1,18 @@
+package mpt.lab.three
+
+/**
+ * @author phpusr
+ *         Date: 22.05.14
+ *         Time: 16:07
+ */
+
+//TODO
+object Matrix {
+
+  val Equals = "="
+
+  val GrammMatrix: Array[Array[String]] = ???
+
+  val GrammRules: Array[String] = ???
+
+}
---
+++ src/main/scala/mpt/lab/three/SyntSymb.scala
@@ -0,0 +1,207 @@
+package mpt.lab.three
+
+import mpt.lab.three.Types.TLexem
+import mpt.lab.three.TSymbKind.TSymbKind
+import mpt.lab.two.lexem.LexType
+import scala.collection.mutable.ListBuffer
+
+/**
+ * @author phpusr
+ *         Date: 22.05.14
+ *         Time: 11:58
+ */
+
+/**
+ * Типы символов: терминальные (лексемы) и нетерминальные
+ */
+object TSymbKind extends Enumeration {
+  type TSymbKind = Value
+
+  val SymbLex = Value
+  val SymbSynt = Value
+}
+
+object Types {
+  type TLexem = LexType
+}
+
+/**
+ * Структура данных для символа грамматики
+ */
+case class TSymbInfo(lexOne: TLexem, lexList: List[TSymbol], symbType: TSymbKind)
+
+
+
+/**
+ * Модуль, обеспечивающий выполнение функции синтаксического
+ * разобра с помощью алгоритма "сдвиг-свертка"
+ */
+object SyntSymb {
+
+  val RuleLength = ???
+
+  /** Сдвиг-свертка */
+  def buildSyntList(listLex: List[TLexem], symbStack: TSymbStack): TSymbol = ???
+
+}
+
+
+/** Массив символов, состовляющих правило грамматики */
+class TSymbArray {
+  val data = new Array[TSymbol](SyntSymb.RuleLength)
+}
+
+/**
+ * Предварительное описание класса "Символ"
+ */
+class TSymbol {
+
+  /** Информация о символе */
+  private var symbInfo: TSymbInfo = null
+  
+  /** Номер правила, которым создан символ */
+  private var iRuleNum: Int = 0
+
+  /** Получение символа из правила по номеру символа */
+  def getItem(symbolIndex: Int): TSymbol = symbInfo.lexList(symbolIndex)
+
+  /** Получение кол-ва символов в правиле */
+  def count: Int = symbInfo.lexList.size
+
+  /** Формирование строкового представления символа */
+  def symbolStr: String = {
+    if (symbType == TSymbKind.SymbSynt) {
+      makeSymbolStr(iRuleNum)
+    } else {
+      lexem.info.get
+    }
+  }
+
+  def makeSymbolStr(ruleNum: Int): String = ???
+
+  /** Тип символа */
+  def symbType = symbInfo.symbType
+
+  /** Ссылка на лексему для терминального символа */
+  def lexem = symbInfo.lexOne
+
+}
+
+object TSymbol {
+
+  /** Создание терминального символа по лексеме */
+  def createLex(lex: TLexem) = {
+    new TSymbol {
+      symbInfo = new TSymbInfo(lex, null, TSymbKind.SymbLex)
+      // Правило не используется пэ 0
+      iRuleNum = 0
+    }
+  }
+
+  /** Создание нетерминального символа на основе правила и массива символов */
+  def createSymb(iR: Int, iSymbN: Int, symbArr: Array[TSymbol]) = {
+    new TSymbol {
+      val lexList = symbArr.reverse.toList
+      symbInfo = new TSymbInfo(null, lexList, TSymbKind.SymbSynt)
+      // Запоминаем номер правила
+      iRuleNum = iR
+    }
+  }
+
+}
+
+/**
+ * Синтаксический стек
+ */
+class TSymbStack {
+
+  private val items = ListBuffer[TSymbol]()
+
+  /** Очистка стека */
+  def clear() = items.clear()
+
+  /** Выборка символа по номеру от вершины стека */
+  def getSymbol(index: Int): TSymbol = items(index)
+
+  /** Помещения в стек входящей лексемы */
+  def push(lex: TLexem): TSymbol = {
+    val lexSymb = TSymbol.createLex(lex)
+    items += lexSymb
+
+    lexSymb
+  }
+
+  /** Самая верхняя лексема в стеке */
+  def topLexem = items.reverse.find(_.symbType == TSymbKind.SymbLex)
+
+  /** Удаление элемента из стека */
+  def delete(index: Int) = items.remove(index)
+
+  /** Свертка и помещение нового символа на вершину стека */
+  def makeTopSymb = {
+    val symbArr: Array[TSymbol] = ???
+    var iSymbN = 0
+    var i = 0
+
+    /** Строка правила */
+    var sRuleStr = ""
+
+    /** Текущий символ */
+    var symCur: TSymbol = null
+
+    val symbols: Array[TSymbol] = ??? //TODO
+
+    val addToRule = (sStr: String, sym: TSymbol) => {
+      symCur = sym
+      symbArr(iSymbN) = symbols(i)
+      sRuleStr += sStr
+      delete(i)
+      iSymbN += 1
+    }
+
+    i = symbols.size
+    symbols.reverse.foreach { s =>
+      i -= 1
+
+      if (s.symbType == TSymbKind.SymbSynt) {
+        addToRule(s.symbolStr, symCur)
+      } else {
+        if (symCur == null) {
+          addToRule(s.lexem.name, s)
+        } else {
+          val rowIndex = s.lexem.name.toInt //TODO
+          val columnIndex = symCur.lexem.name.toInt //TODO
+          if (Matrix.GrammMatrix(rowIndex)(columnIndex) == Matrix.Equals) {
+            addToRule(s.lexem.name, s)
+          } else {
+            //TODO прерываем цикл
+          }
+        }
+      }
+
+      if (iSymbN > SyntSymb.RuleLength) {
+        //TODO прервать цикл
+      }
+    }
+
+    var symbol: Option[TSymbol] = None
+
+    // Если выбран хотя бы один символ из стека
+    if (iSymbN != 0) {
+      val find = Matrix.GrammRules.find(_ == sRuleStr)
+      if (find.isDefined) {
+        symbol = Some(TSymbol.createSymb(i, iSymbN, symbArr))
+        items += symbol.get
+      } else {
+        for (i <- 0 until iSymbN) symbArr(i) = null
+      }
+    }
+
+    symbol
+  }
+
+
+
+}
+
+
