858c51aae8397e8895781161cb2ebff44db6dff4
phpusr <phpusr@gmail.com>
2014-05-23 16:39:24 +0600

    change: Доработка SyntSymb

--- src/main/scala/mpt/lab/three/SyntSymb.scala
+++ src/main/scala/mpt/lab/three/SyntSymb.scala
@@ -44,40 +44,41 @@ object SyntSymb {
 
   /** Сдвиг-свертка */
   def buildSyntList(listLex: List[TLexem], symbStack: TSymbStack): TSymbol = {
-    val iCnt = listLex.size - 1
     val lexStop = LexElem.createInfo("Начало файла", new Position(0, 0, 0))
-
     symbStack.push(lexStop)
-    var i = 0
 
-    var cRule: Char = ???
+    val iCnt = listLex.size - 1
     var result: TSymbol = null
 
-    while (i <= iCnt) {
+    /** Прерывание цикла */
+    var break = false
+
+    var i = 0
+    while (i <= iCnt && !break) {
       val lexTCur = symbStack.topLexem.get.lexInfo
       val lexCurFromList = listLex(i)
 
       if (lexTCur == LexStart && lexCurFromList.lexInfo == LexStart) {
-        // TODO прерываем алгоритм break
-      }
+        break = true
+      } else {
+        // Смотрим отношение лексемы на вершине стека и текущей лексемы в строке
+        var cRule = Matrix.GrammMatrix(lexTCur)(lexCurFromList.lexInfo)
+        cRule = correctRule(cRule, lexTCur, lexCurFromList.lexInfo, symbStack)
 
-      // Смотрим отношение лексемы на вершине стека и текущей лексемы в строке
-      cRule = Matrix.GrammMatrix(lexTCur)(lexCurFromList.lexInfo)
-      cRule = correctRule(cRule, lexTCur, lexCurFromList.lexInfo, symbStack)
-
-      cRule match {
-        case '<' | '=' => // Надо выполнить сдвиг (перенос)
-          symbStack.push(lexCurFromList)
-          i += 1
-        case '>' => // Надо выполнить свертку
-          if (symbStack.makeTopSymb.isEmpty) {
-          // Если не удалось выполнить свертку
-          result = TSymbol.createLex(lexCurFromList)
-          // TODO прерываем алгоритм break
-        } else {
-          // Отношение не установлено - ошибка разбора
-          result = TSymbol.createLex(lexCurFromList)
-          // TODO прерываем алгоритм break
+        cRule match {
+          case '<' | '=' => // Надо выполнить сдвиг (перенос)
+            symbStack.push(lexCurFromList)
+            i += 1
+          case '>' => // Надо выполнить свертку
+            if (symbStack.makeTopSymb.isEmpty) {
+              // Если не удалось выполнить свертку
+              result = TSymbol.createLex(lexCurFromList)
+              break = true
+            } else {
+              // Отношение не установлено - ошибка разбора
+              result = TSymbol.createLex(lexCurFromList)
+              break = true
+            }
         }
       }
     }
