7f86861023f0984915e6068e5be18c91fd288d56
phpusr <phpusr@gmail.com>
2014-05-24 14:47:54 +0600

    add: Правила грамматики

--- src/main/scala/mpt/lab/three/SyntRule.scala
+++ src/main/scala/mpt/lab/three/SyntRule.scala
@@ -40,7 +40,24 @@ object SyntRule {
     Array('<', '<', ' ', ' ', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ')  // Начало
   )
 
-  val GrammRules: Array[String] = ???
+  val E = "E"
+
+  /** Правила грамматики */
+  val GrammRules: Array[Array[String]] = Array(
+    Array(E), // 1
+    Array("if", E, "then", E, "else", E), // 2
+    Array("if", E, "then", E), // 3
+    Array("a", ":=", E), // 4
+    Array("if", E, "then", E, "else", E), // 5
+    Array("a", ":=", E), // 6
+    Array(E, "<", E), // 7
+    Array(E, ">", E), // 8
+    Array(E), // 9
+    Array(E, "==", E), // 10
+    Array(E), // 11
+    Array("a"), // 12
+    Array("(", E, ")") // 13
+  )
 
   /** Корректировка отношения */
   def correctRule(cRule: Char, lexTCur: TLexem, lex: LexType, symbStack: TSymbStack): Char = cRule
